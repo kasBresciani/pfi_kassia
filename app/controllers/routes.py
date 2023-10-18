68c5ba02d37e48835f667abd7c1900a5d44d14fe
kasBresciani <kassia.ifpr.bresciani@gmail.com>
2023-10-17 22:58:48 -0300

    fiz rodape (+-)
    tentei arrumar o rodapé q tá dando erro

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -3,11 +3,19 @@ from flask import render_template
 
 @app.route('/') # o nome da rota pode ser qualquer um
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/cadastro')
 def cadastro():
     return render_template("cadastro.html")
+
+@app.route('/forum')
+def forum():
+    return render_template("forum.html")
+
+# @app.route('/home')
+# def home():
+#     return render_template("home.html")
 
 from app.controllers import UsuarioControler
 
